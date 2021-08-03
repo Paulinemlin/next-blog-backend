1662d5c289eb7b9434e808141d46279f1195bfbf
Pauline <p.emmelin@gmail.com>
2021-08-03 18:05:11 +0200

    first step to add cloudinary

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,25 +1,26 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
+const parse = require("pg-connection-string").parse;
+//const config = parse(process.env.DATABASE_URL);
+// Ajouter config. devant chaque 
 
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
-      options: {
-        ssl: true,
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_UL"));
+  return {
+    defaultConnection: "default",
+    connector: "bookshelf",
+    settings: {
+      client: "postgres",
+      host,
+      port,
+      database,
+      username: user,
+      password,
+      ssl: {
+        rejectUnauthorized: false,
       },
     },
-  },
-});
+    options: {
+      ssl: false,
+    },
+  };
+};
